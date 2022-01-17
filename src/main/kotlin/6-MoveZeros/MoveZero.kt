77e316a64a3b8d3ae025c76398dcddd298092ebf
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2022-01-18 00:27:23 +0530

    PRO-006 | Parikshit | Add description for move zero program

--- src/main/kotlin/6-MoveZeros/MoveZero.kt
+++ src/main/kotlin/6-MoveZeros/MoveZero.kt
@@ -1,5 +1,6 @@
 package `6-MoveZeros`
 //https://leetcode.com/problems/move-zeroes/
+//moveZero using 3 ways : with new array, with lamda or without new array
 class MoveZero {
     fun moveZeroes(inputArray: IntArray): IntArray {
         val size = inputArray.size
