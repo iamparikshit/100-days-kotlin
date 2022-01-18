af762abb07fa5411cd4021cfb0eedac0b2b48c8f
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2022-01-19 00:29:34 +0530

    PRO-007 | Parikshit | Add program to return index no to get the target

--- src/main/kotlin/p7/twoSumArray/TwoSumArray.kt
+++ src/main/kotlin/p7/twoSumArray/TwoSumArray.kt
@@ -1,7 +1,18 @@
 package p7.twoSumArray
-
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 class TwoSumArray {
-    fun twoSum(numbers: IntArray, target: Int): IntArray {
-        return numbers
+    fun twoSum(inputArray: IntArray, target: Int): IntArray {
+        for (index in inputArray.indices) {
+            val secondNo = getSecondNumber(inputArray[index], target)
+            if (inputArray.contains(secondNo)) return intArrayOf(index + 1, inputArray.lastIndexOf(secondNo) + 1)
+        }
+        return inputArray
+    }
+
+    private fun getSecondNumber(firstNumber: Int, target: Int): Int {
+        return target - firstNumber
     }
 }
+
+//below is one of the best ans
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/discuss/1273272/Kotlin-100-faster-and-100-memory-effecient-or-2-pointer-solution-or-O(N)-time-or-Clean-Kotlin-Syntax
--- src/test/kotlin/p7/twoSumArray/TwoSumArrayTest.kt
+++ src/test/kotlin/p7/twoSumArray/TwoSumArrayTest.kt
@@ -1,6 +1,68 @@
 package p7.twoSumArray
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class TwoSumArrayTest {
     val twoSumArray = TwoSumArray()
 
+    @Test
+    fun `should return the array`(){
+        val inputArray = intArrayOf(1,2)
+        val target = 3
+        val result = twoSumArray.twoSum(inputArray,target)
+        val expectedArray = intArrayOf(1,2)
+
+        assertEquals(expectedArray.toList(), result.toList())
+    }
+
+    @Test
+    fun `should return the sum of 5`(){
+        val inputArray = intArrayOf(1,2,3)
+        val target = 5
+        val result = twoSumArray.twoSum(inputArray,target)
+        val expectedArray = intArrayOf(2,3)
+
+        assertEquals(expectedArray.toList(), result.toList())
+    }
+
+    @Test
+    fun `should return the sum of 90`(){
+        val inputArray = intArrayOf(1,2,3,44,46)
+        val target = 90
+        val result = twoSumArray.twoSum(inputArray,target)
+        val expectedArray = intArrayOf(4,5)
+
+        assertEquals(expectedArray.toList(), result.toList())
+    }
+
+    @Test
+    fun `should return the sum of 6`(){
+        val inputArray = intArrayOf(2,3,4)
+        val target = 6
+        val result = twoSumArray.twoSum(inputArray,target)
+        val expectedArray = intArrayOf(1,3)
+
+        assertEquals(expectedArray.toList(), result.toList())
+    }
+
+    @Test
+    fun `should return the sum of -1`(){
+        val inputArray = intArrayOf(-1,0)
+        val target = -1
+        val result = twoSumArray.twoSum(inputArray,target)
+        val expectedArray = intArrayOf(1,2)
+
+        assertEquals(expectedArray.toList(), result.toList())
+    }
+
+    @Test
+    fun `should return the sum of 0`(){
+        val inputArray = intArrayOf(0,0,3,4)
+        val target = 0
+        val result = twoSumArray.twoSum(inputArray,target)
+        val expectedArray = intArrayOf(1,2)
+
+        assertEquals(expectedArray.toList(), result.toList())
+    }
 }
