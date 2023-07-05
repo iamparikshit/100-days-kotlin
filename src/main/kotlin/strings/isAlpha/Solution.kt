1006ad54a370c901807f894c0f5ac84075af85b0
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2023-07-05 20:35:03 +0530

    PRO-031 | Add code to indentify if all characters are alpha numeric

---
+++ src/main/kotlin/strings/isAlpha/Solution.kt
@@ -0,0 +1,11 @@
+package strings.isAlpha
+
+class Solution {
+    fun solve(charArray: CharArray): Int {
+        for(element in charArray){
+            if(element in 'a'..'z' || element in 'A' .. 'Z' || element in '0' .. '9') continue
+            else return 0
+        }
+        return 1
+    }
+}
---
+++ src/test/kotlin/strings/isAlpha/SolutionTest.kt
@@ -0,0 +1,40 @@
+package strings.isAlpha
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class SolutionTest{
+    private val solution = Solution()
+
+    @Test
+     fun `should return 1 when array contains only alphabets`() {
+        val expected = 1
+        val result = solution.solve(charArrayOf('a','A','Z'))
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should return 1 when array contains only digits`() {
+        val expected = 1
+        val result = solution.solve(charArrayOf('1','2','3'))
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should return 1 when array contains only digits and alphabets`() {
+        val expected = 1
+        val result = solution.solve(charArrayOf('1','2','3', 'C', 'b'))
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should return 0 when array contains special characters`() {
+        val expected = 0
+        val result = solution.solve(charArrayOf('1','2','3', 'C', 'b', '#'))
+
+        assertEquals(expected, result)
+    }
+}
