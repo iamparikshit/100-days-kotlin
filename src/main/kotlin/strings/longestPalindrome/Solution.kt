5cecbde32b5b15ff04ae85ae7eac38a720b609ee
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2023-07-09 23:40:37 +0530

    PRO-000 | Add code to identify if string is palindrome

--- src/main/kotlin/strings/longestPalindrome/Solution.kt
+++ src/main/kotlin/strings/longestPalindrome/Solution.kt
@@ -2,9 +2,20 @@ package strings.longestPalindrome
 
 class Solution {
     fun longestPalindrome(inputString: String): String {
-        for(length in inputString.lastIndex downTo 0){
+        return ""
+    }
 
+    fun String.isPalindrome() : Boolean{
+        val length = this.length
+        val mid = (length - 1) / 2
+        var index = 0
+        while(index <= mid ){
+            if(this[index]==this[length-1]) continue
+            else return false
+            index++
         }
+        return true
     }
 }
+
 
--- src/test/kotlin/strings/longestPalindrome/SolutionTest.kt
+++ src/test/kotlin/strings/longestPalindrome/SolutionTest.kt
@@ -37,4 +37,6 @@ class SolutionTest {
 
         assertEquals(expected, result)
     }
+
+
 }
