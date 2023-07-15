0cf2c24ed21dfe1b97ddf7ecbf44b091a7c020ee
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2023-07-15 23:31:05 +0530

    PRO-000 | Refactor : Update logic to check palindrome in string

--- src/main/kotlin/strings/validPalindrome/Solution.kt
+++ src/main/kotlin/strings/validPalindrome/Solution.kt
@@ -4,23 +4,24 @@ class Solution {
     fun isPalindrome(input: String): Boolean {
         val result = StringBuilder()
 
-        for(element in input){
-            if(element in 'a'..'z') result.append(element)
-            else if(element in 'A' .. 'Z'){
-                result.append('a'+ (element - 'A'))
+        for (element in input) {
+            when (element) {
+                in 'a'..'z' -> result.append(element)
+                in 'A'..'Z' -> result.append('a' + (element - 'A'))
+                in '0'..'9' -> result.append(element)
             }
         }
         return result.toString().isValidPalindrome()
     }
 
-    private fun String.isValidPalindrome() : Boolean{
+    private fun String.isValidPalindrome(): Boolean {
         val size = this.length
-        if(size == 0) return true
-        val mid = (size-1)/2
-        var index= 0
-        while(index<=mid){
-            if(this[index]==this[size-1-index])
-            else return false
+        if (size == 0) return true
+        val mid = (size - 1) / 2
+        var index = 0
+        while (index <= mid) {
+            if (this[index] != this[size - 1 - index]) return false
+
             index++
         }
         return true
