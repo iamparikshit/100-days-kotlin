d081ddbce885b72876f2f16e2c584a90cce1abac
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2023-07-16 23:52:51 +0530

    PRO-037 | WIP : Intiate code for converting excel column name to number

---
+++ src/main/kotlin/strings/excelSheetColumnNumber/Solution.kt
@@ -0,0 +1,11 @@
+package strings.excelSheetColumnNumber
+
+class Solution {
+    fun titleToNumber(columnTitle: String): Int {
+        var result = 0
+        for(element in columnTitle){
+           
+        }
+        return  result
+    }
+}
