6e3548232f626fc29a2d5c4976587073db1eafc3
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2023-07-21 21:11:56 +0530

    PRO-000| Refactor

--- src/main/kotlin/strings/truncateSentense/Solution.kt
+++ src/main/kotlin/strings/truncateSentense/Solution.kt
@@ -6,6 +6,7 @@ class Solution {
     fun truncateSentence(inputString: String, count: Int): String {
         val listOfWords = inputString.split(" ")
         val result = StringBuilder()
+        
         for (index in 0 until count){
             if(index==0) result.append(listOfWords[index])
             else result.append(" ${listOfWords[index]}")
