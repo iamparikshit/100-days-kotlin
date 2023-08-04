fe7bc7960f0c506e37cd6656d4d78443cf2876be
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2023-08-04 20:17:28 +0530

    PRO-000 | Initiate program

---
+++ src/test/kotlin/strings/wordBreak/WordBreakTest.kt
@@ -0,0 +1,32 @@
+package strings.wordBreak
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class WordBreakTest{
+    private val wordBreak = WordBreak()
+
+    @Test
+    fun `should return true when string can be made with given combinations`() {
+        val expected = true
+        val result = wordBreak.wordBreak("leetcode", listOf("leet", "code"))
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should return true when applepenapple can be made with given combinations`() {
+        val expected = true
+        val result = wordBreak.wordBreak("applepenapple", listOf("apple", "pen"))
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should return false when string can not be made with given combinations`() {
+        val expected = false
+        val result = wordBreak.wordBreak("catsandog", listOf("cats","dog","sand","and","cat"))
+
+        assertEquals(expected, result)
+    }
+}
